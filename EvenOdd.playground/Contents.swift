a3553ee0999d5c3f604ea2afd2e497480d392389
Pramod Repaka <200411798@student.georgianc.on.ca>
2019-06-01 17:58:11 -0400

    Odd or Even Function

--- EvenOdd.playground/Contents.swift
+++ EvenOdd.playground/Contents.swift
@@ -1,3 +1,47 @@
 import UIKit
 
-var str = "Hello, playground"
+let number = 5
+
+func OddorEvenV1(n1:Int)->String{
+    let remainder = n1 % 2
+    var result: String
+    
+    if (remainder == 0) {
+        result = "Even"
+    }
+    else{
+        result = "Odd"
+    }
+    return result
+}
+
+func OddorEvenV2(_ n1:Int)->String{
+    let remainder = n1 % 2
+    var result: String
+    
+    if (remainder == 0) {
+        result = "Even"
+    }
+    else{
+        result = "Odd"
+    }
+    return result
+}
+
+func OddorEvenV3(num n1:Int)->String{
+    let remainder = n1 % 2
+    var result: String
+    
+    if (remainder == 0) {
+        result = "Even"
+    }
+    else{
+        result = "Odd"
+    }
+    return result
+}
+
+OddorEvenV1(n1: number)
+OddorEvenV2(number)
+OddorEvenV3(num: number)
+
